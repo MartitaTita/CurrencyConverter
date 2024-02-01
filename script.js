88c517b6ffefb83f652645887de42a6bb4249849
Marta Cudzich <>
2024-02-01 15:47:18 +0100

    Add index and style

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-console.log('Hello World!');
+const currencyOne = document.querySelector('#currency-one');
+const amountOne = document.querySelector('.amount-one');
+const currencyTwo = document.querySelector('#currency-two');
+const amountTwo = document.querySelector('.amount-two');
+const swapBtn = document.querySelector('.swap');
+const rateInfo = document.querySelector('.rate-info');
